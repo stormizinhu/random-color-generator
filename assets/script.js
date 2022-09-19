19391f10cfafb9e0af6aa025cc63c026f2480899
Bruno "Stormizinhu" Mocellin <brun0_lp@hotmail.com>
2022-09-19 09:34:20 -0300

    added HEX and HSL colors

--- assets/script.js
+++ assets/script.js
@@ -1,35 +1,79 @@
-function geraCor() {
+function geraRGB() {
     x = Math.floor(Math.random() *255) +1;
     y = Math.floor(Math.random() *255) +1;
     z = Math.floor(Math.random() *255) +1;
     return `rgb(${x}, ${y}, ${z})`
 };
 
+function geraHEX() {
+        let n = (Math.random() * 0xfffff * 1000000).toString(16);
+        return HEX = '#' + n.slice(0, 6);
+};
+
+function geraHSL() {
+    a = Math.floor(Math.random() * 360) +1;
+    b = Math.floor(Math.random() * 100) +1;
+    c = Math.floor(Math.random() * 100) +1;
+    return `hsl(${a}, ${b}%, ${c}%)`;
+}
+
+function trocaFonteRGB() {
+    if (x < 60 && y, z < 200 || y < 60 && x, z < 200 || z < 60 && x, y < 200){
+        return "white";
+    } else {
+        return "black";
+    }
+}
+function trocaFonteHSL() {
+    if (c < 50){
+        return "white";
+    } else {
+        return "black";
+    }
+}
+
 document.getElementById('colorButton').onclick = function () {
-    xLabel.style.backgroundColor = geraCor()
+    xLabel.style.backgroundColor = geraRGB();
     xLabel.innerHTML = `RGB<br> ${x}<br> ${y}<br> ${z}`
     xLabel.style.transition = "ease-in-out 0.5s";
-    if (x < 60 && y, z < 200 || y < 60 && x, z < 200 || z < 60 && x, y < 200){
-        xLabel.style.color = "white";
-    } else {
-        xLabel.style.color = "black";
-    }
+    xLabel.style.color = trocaFonteRGB() ;
 
-    yLabel.style.backgroundColor = geraCor()
+    yLabel.style.backgroundColor = geraRGB();
     yLabel.innerHTML = `RGB<br> ${x}<br> ${y}<br> ${z}`
     yLabel.style.transition = "ease-in-out 0.5s";
-    if (x < 60 && y, z < 200 || y < 60 && x, z < 200 || z < 60 && x, y < 200){
-        yLabel.style.color = "white";
-    } else {
-        yLabel.style.color = "black";
-    }
+    yLabel.style.color = trocaFonteRGB() ;
 
-    zLabel.style.backgroundColor = geraCor()
+    zLabel.style.backgroundColor = geraRGB();
     zLabel.innerHTML = `RGB<br> ${x}<br> ${y}<br> ${z}`
     zLabel.style.transition = "ease-in-out 0.5s";
-    if (x < 60 && y, z < 200 || y < 60 && x, z < 200 || z < 60 && x, y < 200){
-        zLabel.style.color = "white";
-    } else {
-        zLabel.style.color = "black";
-    }
+    zLabel.style.color = trocaFonteRGB() ;
+
+
+    h1Label.style.backgroundColor = geraHEX();
+    h1Label.innerHTML = HEX;
+    h1Label.style.transition = "ease-in-out 0.5s";
+
+    h2Label.style.backgroundColor = geraHEX();
+    h2Label.innerHTML = HEX;
+    h2Label.style.transition = "ease-in-out 0.5s";
+
+    h3Label.style.backgroundColor = geraHEX();
+    h3Label.innerHTML = HEX;
+    h3Label.style.transition = "ease-in-out 0.5s";
+
+    
+    s1Label.style.backgroundColor = geraHSL();
+    s1Label.innerHTML = `HSL<br> ${a}<br> ${b}<br> ${c}`;
+    s1Label.style.transition = "ease-in-out 0.5s";
+    s1Label.style.color = trocaFonteHSL();
+
+    s2Label.style.backgroundColor = geraHSL();
+    s2Label.innerHTML = `HSL<br> ${a}<br> ${b}<br> ${c}`;
+    s2Label.style.transition = "ease-in-out 0.5s";
+    s2Label.style.color = trocaFonteHSL();
+
+    s3Label.style.backgroundColor = geraHSL();
+    s3Label.innerHTML = `HSL<br> ${a}<br> ${b}<br> ${c}`;
+    s3Label.style.transition = "ease-in-out 0.5s";
+    s3Label.style.color = trocaFonteHSL();
 };
